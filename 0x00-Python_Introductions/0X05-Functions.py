4d0a4171c858fd1407e6091a2cea127d8eeb18f9
Samuel Francisco Moncayo Moncayo <110639356+Samuelit00@users.noreply.github.com>
2022-08-10 10:50:23 -0500

    Add files via upload

---
+++ 0x00-Python_Introductions/0X05-Functions.py
@@ -0,0 +1,13 @@
+def sumar(number,number1):
+    print ("Resultado de la suma :", number + number1)
+def restar (number,number1):
+    print ("Resultado de la resta :" , number - number1)
+def multiplicar (number,number1):
+    print ("Resultado de la multiplicacion :", number * number1)
+def potenciar (number,number1):
+    print ("Resultado de la potenciacion", number ** number1)
+
+sumar (10,0)
+restar (100,10)
+multiplicar (100,5)
+potenciar (10,5)
